1851a29be446df53c3a0bd7ec3a0e52eda5671a9
Andreas Triantafyllopoulos <atriant@audeering.com>
2019-05-06 09:25:28 +0200

    added metrics

---
+++ audtorch/metrics/__init__.py
@@ -0,0 +1,9 @@
+r"""To run the examples execute the following commands first::
+
+    >>> import torch
+    >>> from audtorch import metrics
+
+"""
+from .losses import *
+from .metrics import *
+from . import functional
